e0cbaaabb03e89bfe0ffe19b20a12813fab1f363
haiduong149 <dichonline.vn@gmail.com>
2025-08-10 19:07:09 +0700

    Package1updated committed successfully

--- OOP_01_SoPhuc.java
+++ OOP_01_SoPhuc.java
@@ -5,8 +5,8 @@ public class OOP_01_SoPhuc {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SoPhuc x = new SoPhuc();
-		  x.setPhanThuc(14.5);
-		  x.setPhanAo(9);
+		  x.setPhanThuc(14.9);
+		  x.setPhanAo(10);
 		  x.InThongTin();
 		 
 	}
